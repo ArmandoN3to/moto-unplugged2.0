dd769597fc940e73e966e155adf1f419fa7251b0
ArmandoN3to <armandogoncalves2@hotmail.com>
2025-09-03 23:08:05 -0400

    refactor: create model Profile and adjust profile screen

--- app/src/main/java/com/example/motounplugged/MotoUnpluggedApp.kt
+++ app/src/main/java/com/example/motounplugged/MotoUnpluggedApp.kt
@@ -1,17 +1,21 @@
 package com.example.motounplugged
 
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.motounplugged.ui.navigation.AppDrawer
 import com.example.motounplugged.ui.navigation.AppNavHost
 import com.example.motounplugged.ui.navigation.AppScreens
 import kotlinx.coroutines.launch
+
+import com.example.motounplugged.ui.screens.FAB_new_profile
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -54,9 +58,22 @@ fun MotoUnpluggedApp() {
                         }) {
                             Icon(Icons.Default.Menu, contentDescription = "Menu")
                         }
-                    }
+                    },
                 )
+
+
+            },
+            floatingActionButton = {
+                when (selectedRoute) {
+                    AppScreens.Profiles.route-> {
+                        FAB_new_profile {
+                            // Ação ao clicar no FAB
+                            //showToast = true
+                            }
+                    }
+                }
             }
+
         ) { paddingValues ->
             AppNavHost(
                 navController = navController,
--- app/src/main/java/com/example/motounplugged/ui/Screens/ProfilesScreen.kt
+++ app/src/main/java/com/example/motounplugged/ui/Screens/ProfilesScreen.kt
@@ -1,6 +1,8 @@
 package com.example.motounplugged.ui.screens
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items // Importação importante!
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
@@ -13,61 +15,58 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import kotlinx.coroutines.delay
 import com.example.motounplugged.ui.components.ProfileCard
+import com.example.motounplugged.models.Profile
 
-import com.example.motounplugged.ui.components.CustomToast
-import com.example.motounplugged.ui.components.Icon_Edit_Profile
 
 @Composable
 fun ProfilesScreen(modifier: Modifier = Modifier) {
     var showToast by remember { mutableStateOf(false) }
 
-    val cards = listOf(
-        Pair("Trabalho", 4),
-        Pair("Estudo", 6),
-        Pair("Conforto", 6)
+
+
+    val profiles = listOf(
+        Profile("Trabalho", 4),
+        Profile("Estudo", 6),
+        Profile("Academia", 5),
+        Profile("armando", 5),
+        Profile("claudio", 5),
+        Profile("adasd", 5),
+        Profile("adasdfdf", 5)
     )
 
-    Scaffold(
-        floatingActionButton = {
-            FAB_new_profile {
-                // Ação ao clicar no FAB
-                showToast = true
-            }
-        }
-    ) { innerPadding ->
-        Column(
-            modifier = modifier
-                .padding(innerPadding)
-                .fillMaxWidth()
-        ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 30.dp)
-            ) {
-                Text(
-                    text = "Seus Perfis",
-                    fontWeight = FontWeight.ExtraBold,
-                    fontSize = 18.sp,
-                    fontFamily = FontFamily.SansSerif,
-                    color = Color.Black
-                )
-            }
 
-            Column(
-                modifier = Modifier
-                    .padding(horizontal = 20.dp)
-                    .align(Alignment.CenterHorizontally)
+        Box(
+            modifier = modifier
+                .padding()
+                .fillMaxSize(),
+            contentAlignment = Alignment.CenterStart
+
+        ) {
+
+
+            LazyColumn(
+                contentPadding = PaddingValues(horizontal = 10.dp),
+                verticalArrangement = Arrangement.spacedBy(3.dp),
+                modifier = Modifier.fillMaxWidth().padding(),
+                horizontalAlignment = Alignment.CenterHorizontally
+
+
             ) {
-                cards.forEach { (title, count) ->
-                    ProfileCard(title = title, count = count)
+                items(
+                    items = profiles,
+                    key = { profile -> profile.title }
+                ) { profile ->
+                    ProfileCard(
+                        title = profile.title,
+                        count = profile.appCount
+                    )
                 }
             }
         }
     }
-}
+
+
 
 @Composable
 fun FAB_new_profile(onClick: () -> Unit) {
@@ -78,4 +77,9 @@ fun FAB_new_profile(onClick: () -> Unit) {
     ) {
         Icon(Icons.Filled.Add, contentDescription = "Adicionar perfil")
     }
+}
+@Preview
+@Composable
+fun ProfilesScreenPreview() {
+    ProfilesScreen()
 }
