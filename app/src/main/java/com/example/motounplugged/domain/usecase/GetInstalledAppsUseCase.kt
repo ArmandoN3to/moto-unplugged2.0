f64ed1f528a3e1b33d29a45ee31da08766417eb8
ArmandoN3to <armandogoncalves2@hotmail.com>
2025-09-30 15:17:23 -0400

    feat: add use case to get installed apps
    This commit introduces the `GetInstalledAppsUseCase` class.
    
    The `execute` method in this class retrieves a list of installed applications from the device. It filters out system applications and maps the remaining applications to `AppInfo` objects, which include the app's name, package name, and icon. The resulting list is then sorted alphabetically by app name.

---
+++ app/src/main/java/com/example/motounplugged/domain/usecase/GetInstalledAppsUseCase.kt
@@ -0,0 +1,26 @@
+// domain/usecase/GetInstalledAppsUseCase.kt
+package com.example.motounplugged.domain.usecase
+
+import android.content.Context
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
+import com.example.motounplugged.models.AppInfo
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class GetInstalledAppsUseCase(private val context: Context) {
+
+    suspend fun execute(): List<AppInfo> = withContext(Dispatchers.IO) {
+        val packageManager = context.packageManager
+        val apps = packageManager.getInstalledApplications(PackageManager.GET_META_DATA)
+        apps.filter { it.flags and ApplicationInfo.FLAG_SYSTEM == 0 } // Filtra apps de sistema
+            .map {
+                AppInfo(
+                    name = it.loadLabel(packageManager).toString(),
+                    packageName = it.packageName,
+                    icon = it.loadIcon(packageManager)
+                )
+            }
+            .sortedBy { it.name.lowercase() } // Ordena por nome
+    }
+}
