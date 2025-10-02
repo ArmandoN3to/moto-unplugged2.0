cfaa7d1226d84c227ce6e0bff9e5909e2b34b5e5
larissa_andrade <larissa.nogueira.de.andrade@gmail.com>
2025-10-01 20:11:39 -0400

    feature-creat-home-screen-card-profiles

--- app/src/main/java/com/example/motounplugged/ui/features/home/HomeScreen.kt
+++ app/src/main/java/com/example/motounplugged/ui/features/home/HomeScreen.kt
@@ -12,10 +12,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccessTime
+import androidx.compose.material.icons.filled.AppShortcut
 import androidx.compose.material3.Button
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
@@ -24,6 +30,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -72,24 +79,23 @@ fun HomeScreen(
         }
 
 
-
+       //card de perfis
         OutlinedCard (
             border = BorderStroke(1.dp,Color.Black),
             modifier = Modifier
-                .size(width = 350.dp, height = 150.dp)
+                .size(width = 350.dp, height = 180.dp)
                 .align(Alignment.CenterHorizontally)
-                .padding(top = 5.dp)
+                .padding(top = 10.dp)
         ){
             Row {
                 Text(
                     text = "Trabalho",
-                    fontStyle = FontStyle.Normal,
+                    fontStyle = FontStyle.Italic,
                     fontSize = 20.sp,
                     color = Color.Black,
                     fontWeight = FontWeight.Bold,
                     modifier = Modifier
                         .padding(16.dp)
-
                 )
 
                 Spacer(
@@ -99,7 +105,7 @@ fun HomeScreen(
 
                 ElevatedButton(onClick = { onClick() },
                     modifier = Modifier
-                        .padding(0.dp)) {
+                        .padding(horizontal = 10.dp)) {
                     Text(
                         text = "Ativo",
                         fontWeight = FontWeight.Bold,
@@ -110,26 +116,96 @@ fun HomeScreen(
                             .padding(1.dp)
                     )
                 }
-
+            }
+            // linha de descrição de modo
+            Row (
+            ){
+                Text(
+                    text = "Bloqueie redes sociais",
+                    fontStyle = FontStyle.Italic,
+                    fontSize = 14.sp,
+                    color = Color.Gray,
+                    modifier = Modifier
+                        .padding(top = 0.dp)
+                        .padding(start = 16.dp)
+                )
             }
 
             Row {
-
-                Text(
-                    text = "Bloqueie redes sociais",
-                    fontStyle = FontStyle.Normal,
-                    fontSize = 20.sp,
-                    color = Color.Gray,
+                Icon(
+                    imageVector = Icons.Default.AccessTime,
+                    contentDescription = "Hash symbol",
                     modifier = Modifier
-                        .padding(top = 6.dp)
-
+                        .size(width = 30.dp, height = 18.dp)
+                        .padding(start = 16.dp)
+                        .padding(top = 7.dp)
                 )
+                Text(
+                    text = "2h",
+                    fontSize = 10.sp,
+                    fontStyle = FontStyle.Italic,
+                    color = Color.Black
+                )
+
+
+                Icon(
+                    imageVector = Icons.Default.AppShortcut,
+                    contentDescription = "Hash symbol",
+                    modifier = Modifier
+                        .size(width = 30.dp, height = 18.dp)
+                        .padding(start = 16.dp)
+                        .padding(top = 7.dp)
+                )
+                Text(
+                    text = "12 apps ",
+                    fontSize = 10.sp,
+                    fontStyle = FontStyle.Italic,
+                    color = Color.Black
+                )
+            }
+                ElevatedCard(
+                    shape = RoundedCornerShape(5.dp),
+                    elevation = CardDefaults.cardElevation(
+                        defaultElevation = 6.dp
+                    ),
+                    modifier = Modifier
+                        .size(width = 270.dp, height = 50.dp)
+                        .padding(start = 15.dp, top = 5.dp)
+
+                ) {
+                    Row {
+                        Text(
+                            text = "Horário da sessão",
+                            fontSize = 10.sp,
+                            fontStyle = FontStyle.Italic,
+                            color = Color.Gray,
+                            modifier = Modifier
+                                .padding(start = 5.dp),
+                            textAlign = TextAlign.Center,
+                        )
+                    }
+                    Row {
+                        Text(
+                            text = "14:00 -16:00",
+                            fontSize = 10.sp,
+                            fontStyle = FontStyle.Italic,
+                            color = Color.Gray,
+                            modifier = Modifier
+                                .padding(start = 6.dp, end = 10.dp),
+                            textAlign = TextAlign.Center,
+                        )
+
+                    }
+
+
+
+                }
+
             }
 
 
         }
     }
-}
 
 @Preview(showBackground = true
 )
