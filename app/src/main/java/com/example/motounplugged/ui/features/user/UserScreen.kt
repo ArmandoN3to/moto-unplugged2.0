2a14e6863c57379aead50815511dd0b78ddaaf34
larissa_andrade <larissa.nogueira.de.andrade@gmail.com>
2025-10-27 20:13:17 -0400

    feature componentes name and email

--- app/src/main/java/com/example/motounplugged/ui/features/user/UserScreen.kt
+++ app/src/main/java/com/example/motounplugged/ui/features/user/UserScreen.kt
@@ -8,9 +8,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -29,14 +36,40 @@ fun UserScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+            .padding(50.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text("Selecione sua foto", fontSize = 20.sp)
         Spacer(modifier = Modifier.height(16.dp))
         PhotoPicker()
+
+        Column (modifier = Modifier){
+            var text by remember { mutableStateOf("") }
+            var text1 by remember { mutableStateOf("") }
+
+            TextField(
+                value = text,
+                onValueChange = { text = it },
+                label = { Text("Nome") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 50.dp)
+            )
+            TextField(
+                value = text1,
+                onValueChange = { text1= it },
+                label = { Text("Email") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 15.dp)
+            )
+
+
+
+        }
     }
+
+
 }
 
 @Preview(showBackground = true)
