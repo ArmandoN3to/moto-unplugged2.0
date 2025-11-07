566d8e7603801605185cc2f026c5d574878ef75d
larissngg <larissa.nogueira.de.andrade@gmail.com>
2025-11-07 16:16:38 -0400

    refactor-user-screen

--- app/src/main/java/com/example/motounplugged/ui/features/user/UserScreen.kt
+++ app/src/main/java/com/example/motounplugged/ui/features/user/UserScreen.kt
@@ -1,81 +1,71 @@
-package com.example.motounplugged.ui.features.user
+package com.example.motounplugged.ui.features.settings
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.motounplugged.ui.components.PhotoPicker
-import com.example.motounplugged.ui.theme.MotoUnpluggedTheme
 
 @Composable
 fun UserScreen(
     modifier: Modifier = Modifier
 ){
-    Column(
+    Column (
         modifier = Modifier
+            .padding(horizontal = 16.dp)
             .fillMaxSize()
-            .padding(50.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text("Selecione sua foto", fontSize = 20.sp)
-        Spacer(modifier = Modifier.height(16.dp))
-        PhotoPicker()
-
-        Column (modifier = Modifier){
-            var text by remember { mutableStateOf("") }
-            var text1 by remember { mutableStateOf("") }
-
-            TextField(
-                value = text,
-                onValueChange = { text = it },
-                label = { Text("Nome") },
+            .verticalScroll(rememberScrollState())
+    ){
+        Column (
+            modifier = Modifier
+                .fillMaxWidth()
+        ){
+            Text(
+                text = "Usuário",
+                fontSize = 25.sp,
+                fontStyle = FontStyle.Normal,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black,
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 50.dp)
+                    .align(Alignment.CenterHorizontally)
             )
-            TextField(
-                value = text1,
-                onValueChange = { text1= it },
-                label = { Text("Email") },
+            Text(
+                text = "Gerencie sua conta e ajustes",
+                fontSize = 20.sp,
+                fontStyle = FontStyle.Normal,
+                color = Color.Gray,
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 15.dp)
+                    .padding(top = 5.dp)
+                    .align(Alignment.CenterHorizontally)
             )
-
-
-
         }
-    }
 
+        Column (
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "*Tela em desenvolvimento*",
+                fontSize = 15.sp,
+                fontStyle =  FontStyle.Normal,
+                color = Color.Gray,
+                modifier = Modifier
+                    .padding(top = 250.dp)
+            )
+        }
 
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun Userscreenpreview(){
-    MotoUnpluggedTheme {
-        UserScreen()
     }
 }
