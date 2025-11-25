4b22389ade0b8c5021a3e7f8b2d348fedbc86453
armando_n3to <armandogoncalves2@hotmail.com>
2025-11-24 20:24:36 -0400

    feat: add about settings screen

---
+++ app/src/main/java/com/example/motounplugged/ui/features/settings/about/AboutSettingsScreen.kt
@@ -0,0 +1,63 @@
+package com.example.motounplugged.ui.features.settings.about
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.motounplugged.ui.components.NavigationSettingsCard
+import com.example.motounplugged.ui.components.GeneralSettingsCard
+import com.example.motounplugged.ui.components.AboutApp
+
+
+@Composable
+fun AboutSettingsScreen(
+    modifier: Modifier = Modifier
+){
+    Column (
+        modifier = Modifier
+            .padding(horizontal = 16.dp)
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+    ){
+        Column (
+            modifier = Modifier
+                .fillMaxWidth()
+        ){
+            Text(
+                text = "Sobre",
+                fontSize = 25.sp,
+                fontStyle = FontStyle.Normal,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+            )
+            Text(
+                text = "Gerencie suas configurações",
+                fontSize = 20.sp,
+                fontStyle = FontStyle.Normal,
+                color = Color.Gray,
+                modifier = Modifier
+                    .padding(top = 5.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
+        }
+        AboutApp {
+            println("Clicou no AboutApp")
+
+        }
+
+    }
+}
