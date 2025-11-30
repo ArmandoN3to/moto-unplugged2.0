8d3d9b71aa5b9968d6f299d8680bc4dfe9bec6ef
larissa_andrade <larissa.nogueira.de.andrade@gmail.com>
2025-11-30 15:43:29 -0400

    refactor-user-screen-delete-reajust-in-navhost-appscreens

--- app/src/main/java/com/example/motounplugged/ui/navigation/AppScreens.kt
+++ app/src/main/java/com/example/motounplugged/ui/navigation/AppScreens.kt
@@ -6,7 +6,6 @@ sealed class AppScreens(val route: String) {
     object Schedule : AppScreens("schedule")
     object Stats : AppScreens("stats")
     object Profiles : AppScreens("profiles")
-    object User : AppScreens("user")
     object Settings : AppScreens("settings")
     object Streak : AppScreens("streak")
     object CreateProfile : AppScreens("create_profile")
