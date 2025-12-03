69c8b815ec98e581e7d4370fbec91fa0684af0b9
williammreis <contato.williamandroeza@gmail.com>
2025-12-03 19:45:39 -0400

    Revert "Alteração do campo senha"
    This reverts commit 5662f74bb16822b4f7e810e39c5d1ebba28d9ee3.
    
    Retorna ao anterior sem autenticação de senha

--- app/src/main/java/com/example/motounplugged/MainActivity.kt
+++ app/src/main/java/com/example/motounplugged/MainActivity.kt
@@ -5,11 +5,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.fragment.app.FragmentActivity
 import com.example.motounplugged.ui.theme.MotoUnpluggedTheme
 //import androidx.core.splashscreen.installSplashScreen
 
-class MainActivity : FragmentActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/motounplugged/ui/features/createprofile/CreateProfileScreen.kt
+++ app/src/main/java/com/example/motounplugged/ui/features/createprofile/CreateProfileScreen.kt
@@ -41,13 +41,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.core.content.ContextCompat
-import androidx.fragment.app.FragmentActivity
 import coil.compose.AsyncImage
-import androidx.biometric.BiometricPrompt
-
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -126,33 +121,6 @@ private fun CreateProfileContent(
         uri?.let {
             onEvent(CreateProfileEvent.OnWallpaperSelected(uri.toString()))
         }
-    }
-
-    val context = LocalContext.current
-    val activity = context as FragmentActivity
-
-    // Launcher Biométrico
-    fun requestAuthentication(onSuccess: () -> Unit) {
-        val executor = ContextCompat.getMainExecutor(context)
-
-        val biometricPrompt = BiometricPrompt(
-            activity,
-            executor,
-            object : BiometricPrompt.AuthenticationCallback() {
-                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
-                    super.onAuthenticationSucceeded(result)
-                    onSuccess()
-                }
-            }
-        )
-
-        val promptInfo = BiometricPrompt.PromptInfo.Builder()
-            .setTitle("Confirmação necessária")
-            .setSubtitle("Use sua digital ou senha do aparelho para confirmar")
-            .setNegativeButtonText("Cancelar")
-            .build()
-
-        biometricPrompt.authenticate(promptInfo)
     }
 
 
@@ -274,16 +242,7 @@ private fun CreateProfileContent(
         Spacer(Modifier.height(16.dp))
 
         Button(
-            onClick = {
-                if (uiState.passwordRequired) {
-                    // Pedir autorização biométrica antes de salvar
-                    requestAuthentication {
-                        onEvent(CreateProfileEvent.OnSaveProfileClick)
-                    }
-                } else {
-                    onEvent(CreateProfileEvent.OnSaveProfileClick)
-                }
-            },
+            onClick = { onEvent(CreateProfileEvent.OnSaveProfileClick) },
             modifier = Modifier.fillMaxWidth(),
             enabled = uiState.profileName.isNotBlank() && !uiState.isLoading
         ) {
